31010227b77d5bb33ba0289ad85b3536841a9ad6
3ddesign <3ddesign@bk.ru>
2018-11-11 09:04:25 +0200

    test

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,12 +1,12 @@
-const AWS = require('aws-sdk');
+// const AWS = require('aws-sdk');
 const uuid = require('uuid/v1');
 const requireLogin = require('../middlewares/requireLogin');
 const keys = require('../config/keys');
 
-const s3 = new AWS.S3({
-  accessKeyId: keys.accessKeyId,
-  secretAccessKey: keys.secretAccessKey
-});
+// const s3 = new AWS.S3({
+//   accessKeyId: keys.accessKeyId,
+//   secretAccessKey: keys.secretAccessKey
+// });
 
 module.exports = app => {
   app.get('/api/upload', requireLogin, (req, res) => {
