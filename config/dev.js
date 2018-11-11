ec3d1b9416b82970e0e344da4ae1e387316cd99a
3ddesign <3ddesign@bk.ru>
2018-11-11 08:46:47 +0200

    add file upload

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://admin:123qweasd@ds131373.mlab.com:31373/blogdev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAJZPXJLJYJUIFBL2A', 
+  secretAccessKey: ''
 };
